b8fc98d03dd2af542ac43c1b84764b683a879b8d
HeyItsShuga <Shugabuga@users.noreply.github.com>
2017-04-03 11:55:42 -0400

    Add Hollr and S0n1c's server

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ var __apiKeys = [
 	'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
 	'm778874125-2c85b6d2609bca9ca1dc8b1a', // jCO Black
 	'm778874090-6598e17eab090ac08b2a44f1', // DevSquad
+   'm778876873-fbd787eaccec27e4156a1719', // Hollr/S0n1c
 	'm778874126-2199efa94e371583fd42a7e7' // fontPages
 ];
 
