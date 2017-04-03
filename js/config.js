c810a4374014d2859aeae14931c00fefc8f7afa9
Shugabuga <jeffreydivi@icloud.com>
2017-04-02 21:43:00 -0400

    GitIgnore...

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm778874079-084384dfb127480a93598c07', // HeyItsShuga Black
+	'm778874080-8f05a8f2fba498b74b05b06d', // HeyitShuga Blue
+	'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
+	'm778874125-2c85b6d2609bca9ca1dc8b1a', // jCO Black
+	// 'm778874090-6598e17eab090ac08b2a44f1', // DevSquad
+	'm778874126-2199efa94e371583fd42a7e7' // fontPages
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
