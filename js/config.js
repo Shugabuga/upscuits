47fcd24a0dacdec8742f2a2ec56f375929d2676c
HeyItsShuga <Shugabuga@users.noreply.github.com>
2018-02-23 17:47:34 -0500

    Fixes

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm778959290-aec903f88269aa3621ba3ad5', // shuga.co
-	'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
-	'm778874090-6598e17eab090ac08b2a44f1', // DevSquad
-   'm778876873-fbd787eaccec27e4156a1719', // warsame.pro
-	'm778874126-2199efa94e371583fd42a7e7', // fontPages
+    'm778959290-aec903f88269aa3621ba3ad5', // shuga.co
+    'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
+    'm778874090-6598e17eab090ac08b2a44f1', // DevSquad
+    'm778876873-fbd787eaccec27e4156a1719', // s0n1c.org
+    'm778874126-2199efa94e371583fd42a7e7', // fontPages
     'm779352438-ea9a9b3164e5aafb837d67e9', // iEMU
-    'm779352445-0cfcb376793aa9bbd72416d8' // Alexa Skills
+    'm779352445-0cfcb376793aa9bbd72416d8', // Alexa Skills
+    'm780045346-2d77165d0776d2d1c7f1c77d'  // EclipseEmu 
 ];
 
 // refresh interval (in seconds)
