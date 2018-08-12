f56476ca37777290bc93bb605de94fe5f26b78bc
HeyItsShuga <Shugabuga@users.noreply.github.com>
2018-08-12 17:22:13 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,14 +2,13 @@
 var __apiKeys = [
     'm778959290-aec903f88269aa3621ba3ad5', // shuga.co
     'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
-    'm778874090-6598e17eab090ac08b2a44f1', // DevSquad
-    'm778876873-fbd787eaccec27e4156a1719', // s0n1c.org
+    'm778876873-fbd787eaccec27e4156a1719', // s0n1c.org (Front End)
     'm778874126-2199efa94e371583fd42a7e7', // fontPages
     'm779352438-ea9a9b3164e5aafb837d67e9', // iEMU
-    'm779352445-0cfcb376793aa9bbd72416d8', // Alexa Skills
     'm780210676-cf34547cd8284cd2a5c4bf4c', // IndigoHub: library (www)
     'm780210670-f51a3a3d619706ed7e423a9f', // IndigoHub: app
-    'm780045346-2d77165d0776d2d1c7f1c77d'  // EclipseEmu 
+    'm780045346-2d77165d0776d2d1c7f1c77d', // Eclipse (Front End)
+    'm780822745-dc7ca6fbee3c28549ad8a079'  // Eclipse (Back End)
 ];
 
 // refresh interval (in seconds)
