bce3d8492fd34421dedb3505d4b304f8b0205657
HeyItsShuga <Shugabuga@users.noreply.github.com>
2018-09-08 09:45:16 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,12 +4,11 @@ var __apiKeys = [
     'm778874106-d364e78085e7c4a22ff7d38a', // /u/TheMirrorPool
     'm778876873-fbd787eaccec27e4156a1719', // s0n1c.org (Front End)
     'm778874126-2199efa94e371583fd42a7e7', // fontPages
-    'm779352438-ea9a9b3164e5aafb837d67e9', // iEMU
-    'm780210676-cf34547cd8284cd2a5c4bf4c', // IndigoHub: library (www)
-    'm780210670-f51a3a3d619706ed7e423a9f', // IndigoHub: app
     'm780045346-2d77165d0776d2d1c7f1c77d', // Eclipse (Front End)
     'm780822745-dc7ca6fbee3c28549ad8a079'  // Eclipse (Back End)
 ];
+
+//    'm779352438-ea9a9b3164e5aafb837d67e9', // iEMU - old and probably will stay offline.
 
 // refresh interval (in seconds)
 var __refresh = 300;
